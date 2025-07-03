415854aa4968e9f23368819008c8054049e6e67e
Carlos Velasquez <cvelasquezr8@gmail.com>
2025-07-03 13:41:09 -0500

    Update Swagger configuration to be available in production environment

--- Program.cs
+++ Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
